4bb38d561146ca0d36f409822438467f8026d5b1
Riku Higa <e195760@ie.u-ryukyu.ac.jp>
2019-11-28 22:50:35 +0900

    ステップ2が完了したのでcommit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args){
-        try{
+        /*try{
             String str=null;
             System.out.println(str.length());
         }catch (NullPointerException e){
             System.out.println("例外NullPointerExceptionが発生しました");
             System.out.println(e.getMessage());
-        }
-
+        }*///Step2まで
+        String str="百二十三";
+        int value;
+        value=Integer.parseInt(str);
     }
 }
